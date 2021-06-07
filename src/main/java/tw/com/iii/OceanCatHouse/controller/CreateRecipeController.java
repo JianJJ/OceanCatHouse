4aaaa48c4ef82b956975841278728f0a141caa3f
Louis JJJ <louis82113@hotmail.com>
2021-06-07 21:48:48 +0800

    jain 0607

---
+++ src/main/java/tw/com/iii/OceanCatHouse/controller/CreateRecipeController.java
@@ -0,0 +1,21 @@
+package tw.com.iii.OceanCatHouse.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController()
+public class CreateRecipeController {
+
+    @GetMapping("/{RecipeName}")
+    public ModelAndView createRecipe(@PathVariable("RecipeName")String RecipeName){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject(RecipeName);
+        modelAndView.setViewName("/pages/jiantest01");
+        return modelAndView;
+    }
+}
