da919d5b70250969f2eefa32c6ebc5bd23070e9d
Louis JJJ <louis82113@hotmail.com>
2021-06-07 20:18:16 +0800

    new Maven Project

---
+++ src/main/java/tw/com/iii/OceanCatHouse/OceanCatHouseApplication.java
@@ -0,0 +1,13 @@
+package tw.com.iii.OceanCatHouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceanCatHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OceanCatHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tw/com/iii/OceanCatHouse/OceanCatHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.com.iii.OceanCatHouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OceanCatHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
