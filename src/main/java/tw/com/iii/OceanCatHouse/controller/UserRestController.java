d5a618f251b6e0f1dcbceb95bbeafadeeb257af9
zero71029 <wiz71029@gmail.com>
2021-06-09 23:51:15 +0800

    zero0609

--- src/main/java/tw/com/iii/OceanCatHouse/controller/UserRestController.java
+++ src/main/java/tw/com/iii/OceanCatHouse/controller/UserRestController.java
@@ -1,6 +1,7 @@
 package tw.com.iii.OceanCatHouse.controller;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -8,19 +9,28 @@ import java.net.HttpURLConnection;
 import java.net.InetAddress;
 import java.net.URL;
 import java.net.UnknownHostException;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+
 
 import javax.servlet.http.HttpSession;
 
 import org.json.JSONObject;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import tw.com.iii.OceanCatHouse.model.UserBean;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+
+
+import com.google.api.client.json.gson.GsonFactory;
+
+
 
 @RestController
 public class UserRestController {
@@ -83,4 +93,63 @@ public class UserRestController {
 		}
 		return null;
 	}
+	
+	
+	
+	
+	
+	
+	@RequestMapping("/oauth")
+	public void name( String idtoken) {
+		GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
+			    // Specify the CLIENT_ID of the app that accesses the backend:
+			    .setAudience(Collections.singletonList("849367464998-0c4najofsqmh3rteejq2dc3va9iqdps2.apps.googleusercontent.com"))
+			    // Or, if multiple clients access the backend:
+			    //.setAudience(Arrays.asList(CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3))
+			    .build();
+
+			// (Receive idTokenString by HTTPS POST)
+
+			GoogleIdToken idToken=null;
+			try {
+				idToken = verifier.verify(idtoken);
+			} catch (GeneralSecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (idToken != null) {
+			  Payload payload = idToken.getPayload();
+
+			  // Print user identifier
+			  String userId = payload.getSubject();
+			  System.out.println("User ID: " + userId);
+
+			  // Get profile information from payload
+			  String email = payload.getEmail();
+			  boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
+			  String name = (String) payload.get("name");
+			  String pictureUrl = (String) payload.get("picture");
+			  String locale = (String) payload.get("locale");
+			  String familyName = (String) payload.get("family_name");
+			  String givenName = (String) payload.get("given_name");
+			  System.out.println("User name: " + name);
+			  System.out.println("User email: " + email);
+			  System.out.println("User locale: " + locale);
+			  System.out.println("User givenName: " + givenName);
+			  
+			  
+			  
+			  
+			  // Use or store profile information
+			  // ...
+
+			} else {
+			  System.out.println("Invalid ID token.");
+			}
+	}
+	
+	
 }
--- src/main/webapp/WEB-INF/index.jsp
+++ src/main/webapp/WEB-INF/index.jsp
@@ -1,19 +1,59 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+<meta name="google-signin-client_id"
+	content="849367464998-0c4najofsqmh3rteejq2dc3va9iqdps2.apps.googleusercontent.com">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Home</title>
 </head>
 <body>
 
-<h3>Welcome ${user.custid}</h3>
+	<h3>Welcome ${user.custid}</h3>
 
-<h3><a href="<c:url value="/views/secure/login.jsp" />">Login</a></h3>
-<h3><a href="<c:url value="/views/pages/product.jsp" />">Product</a></h3>
-<h3><a href="<c:url value="/views/login" />">login</a></h3>
-<h3><a href="<c:url value="/views/signup" />">signup</a></h3>
+	<h3>
+		<a href="<c:url value="/views/secure/login.jsp" />">Login</a>
+	</h3>
+	<h3>
+		<a href="<c:url value="/views/pages/product.jsp" />">Product</a>
+	</h3>
+	<h3>
+		<a href="<c:url value="/views/login" />">login</a>
+	</h3>
+	<h3>
+		<a href="<c:url value="/views/signup" />">signup</a>
+	</h3>
+	<div class="g-signin2" data-onsuccess="onSignIn"></div>
+	<a href="#" onclick="signOut();">Sign out</a>
+	<script type="text/javascript">
+		function onSignIn(googleUser) {
+			//取得資料
+			var profile = googleUser.getBasicProfile();
+			console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+			console.log('Name: ' + profile.getName());
+			console.log('Image URL: ' + profile.getImageUrl());
+			console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+			var id_token = googleUser.getAuthResponse().id_token;
+			//發送
+			var xhr = new XMLHttpRequest();
+			xhr.open('POST', '/recipe/oauth');
+			xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+			xhr.onload = function() {
+			  console.log('Signed in as: ' + xhr.responseText);
+			};
+			xhr.send('idtoken=' + id_token);
+		}
+	</script>
+	<script>
+		function signOut() {
+			var auth2 = gapi.auth2.getAuthInstance();
+			auth2.signOut().then(function() {
+				console.log('User signed out.');
+			});
+		}
+	</script>
 </body>
 </html>
