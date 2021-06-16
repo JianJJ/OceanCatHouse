3568b29dde3d8462b611ed9711a677313b6aff24
Louis JJJ <louis82113@hotmail.com>
2021-06-16 23:22:33 +0800

    jian0616_1122

--- src/main/java/tw/com/iii/OceanCatHouse/model/RecipeMainBean.java
+++ src/main/java/tw/com/iii/OceanCatHouse/model/RecipeMainBean.java
@@ -27,262 +27,6 @@ public class RecipeMainBean {
     private String RecCreated;
     private Integer RecViews;
 
-    public Integer getRecId() {
-        return RecId;
-    }
-
-    public void setRecId(Integer recId) {
-        RecId = recId;
-    }
-
-    public Integer getCategoryId() {
-        return CategoryId;
-    }
-
-    public void setCategoryId(Integer categoryId) {
-        CategoryId = categoryId;
-    }
-
-    public String getRecTitle() {
-        return RecTitle;
-    }
-
-    public void setRecTitle(String recTitle) {
-        RecTitle = recTitle;
-    }
-
-    public String getRecPic() {
-        return RecPic;
-    }
-
-    public void setRecPic(String recPic) {
-        RecPic = recPic;
-    }
-
-    public String getRecText() {
-        return RecText;
-    }
-
-    public void setRecText(String recText) {
-        RecText = recText;
-    }
-
-    public Integer getRecTime() {
-        return RecTime;
-    }
-
-    public void setRecTime(Integer recTime) {
-        RecTime = recTime;
-    }
-
-    public Integer getRecNum() {
-        return RecNum;
-    }
-
-    public void setRecNum(Integer recNum) {
-        RecNum = recNum;
-    }
-
-    public Integer getRecCal() {
-        return RecCal;
-    }
-
-    public void setRecCal(Integer recCal) {
-        RecCal = recCal;
-    }
-
-    public String getRecTag() {
-        return RecTag;
-    }
-
-    public void setRecTag(String recTag) {
-        RecTag = recTag;
-    }
-
-    public Integer getUserId() {
-        return UserId;
-    }
-
-    public void setUserId(Integer userId) {
-        UserId = userId;
-    }
-
-    public Integer getRecLiked() {
-        return RecLiked;
-    }
-
-    public void setRecLiked(Integer recLiked) {
-        RecLiked = recLiked;
-    }
-
-    public String getRecCreated() {
-        return RecCreated;
-    }
-
-    public void setRecCreated(String recCreated) {
-        RecCreated = recCreated;
-    }
-
-    public Integer getRecViews() {
-        return RecViews;
-    }
-
-    public void setRecViews(Integer recViews) {
-        RecViews = recViews;
-    }
-
-    public RecipeCategoryBean getRecipeCategoryBean() {
-        return recipeCategoryBean;
-    }
-
-    public void setRecipeCategoryBean(RecipeCategoryBean recipeCategoryBean) {
-        this.recipeCategoryBean = recipeCategoryBean;
-    }
-
-    public Set<RecipeMaterialBean> getRecipeMaterialBeans() {
-        return recipeMaterialBeans;
-    }
-
-    public void setRecipeMaterialBeans(Set<RecipeMaterialBean> recipeMaterialBeans) {
-        this.recipeMaterialBeans = recipeMaterialBeans;
-    }
-
-    public Set<RecipeStepBean> getRecipeStepBeans() {
-        return recipeStepBeans;
-    }
-
-    public void setRecipeStepBeans(Set<RecipeStepBean> recipeStepBeans) {
-        this.recipeStepBeans = recipeStepBeans;
-    }
-
-    public Integer getRecId() {
-        return RecId;
-    }
-
-    public void setRecId(Integer recId) {
-        RecId = recId;
-    }
-
-    public Integer getCategoryId() {
-        return CategoryId;
-    }
-
-    public void setCategoryId(Integer categoryId) {
-        CategoryId = categoryId;
-    }
-
-    public String getRecTitle() {
-        return RecTitle;
-    }
-
-    public void setRecTitle(String recTitle) {
-        RecTitle = recTitle;
-    }
-
-    public String getRecPic() {
-        return RecPic;
-    }
-
-    public void setRecPic(String recPic) {
-        RecPic = recPic;
-    }
-
-    public String getRecText() {
-        return RecText;
-    }
-
-    public void setRecText(String recText) {
-        RecText = recText;
-    }
-
-    public Integer getRecTime() {
-        return RecTime;
-    }
-
-    public void setRecTime(Integer recTime) {
-        RecTime = recTime;
-    }
-
-    public Integer getRecNum() {
-        return RecNum;
-    }
-
-    public void setRecNum(Integer recNum) {
-        RecNum = recNum;
-    }
-
-    public Integer getRecCal() {
-        return RecCal;
-    }
-
-    public void setRecCal(Integer recCal) {
-        RecCal = recCal;
-    }
-
-    public String getRecTag() {
-        return RecTag;
-    }
-
-    public void setRecTag(String recTag) {
-        RecTag = recTag;
-    }
-
-    public Integer getUserId() {
-        return UserId;
-    }
-
-    public void setUserId(Integer userId) {
-        UserId = userId;
-    }
-
-    public Integer getRecLiked() {
-        return RecLiked;
-    }
-
-    public void setRecLiked(Integer recLiked) {
-        RecLiked = recLiked;
-    }
-
-    public String getRecCreated() {
-        return RecCreated;
-    }
-
-    public void setRecCreated(String recCreated) {
-        RecCreated = recCreated;
-    }
-
-    public Integer getRecViews() {
-        return RecViews;
-    }
-
-    public void setRecViews(Integer recViews) {
-        RecViews = recViews;
-    }
-
-//    public RecipeCategoryBean getRecipeCategoryBean() {
-//        return recipeCategoryBean;
-//    }
-//
-//    public void setRecipeCategoryBean(RecipeCategoryBean recipeCategoryBean) {
-//        this.recipeCategoryBean = recipeCategoryBean;
-//    }
-//
-//    public Set<RecipeMaterialBean> getRecipeMaterialBeans() {
-//        return recipeMaterialBeans;
-//    }
-//
-//    public void setRecipeMaterialBeans(Set<RecipeMaterialBean> recipeMaterialBeans) {
-//        this.recipeMaterialBeans = recipeMaterialBeans;
-//    }
-//
-//    public Set<RecipeStepBean> getRecipeStepBeans() {
-//        return recipeStepBeans;
-//    }
-//
-//    public void setRecipeStepBeans(Set<RecipeStepBean> recipeStepBeans) {
-//        this.recipeStepBeans = recipeStepBeans;
-//    }
-
     @Override
     public String toString() {
         return "RecipeMainBean{" +
@@ -304,6 +48,111 @@ public class RecipeMainBean {
                 ", recipeStepBeans=" + recipeStepBeans +
                 '}';
     }
+
+    public Integer getRecId() {
+        return RecId;
+    }
+
+    public void setRecId(Integer recId) {
+        RecId = recId;
+    }
+
+    public Integer getCategoryId() {
+        return CategoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        CategoryId = categoryId;
+    }
+
+    public String getRecTitle() {
+        return RecTitle;
+    }
+
+    public void setRecTitle(String recTitle) {
+        RecTitle = recTitle;
+    }
+
+    public String getRecPic() {
+        return RecPic;
+    }
+
+    public void setRecPic(String recPic) {
+        RecPic = recPic;
+    }
+
+    public String getRecText() {
+        return RecText;
+    }
+
+    public void setRecText(String recText) {
+        RecText = recText;
+    }
+
+    public Integer getRecTime() {
+        return RecTime;
+    }
+
+    public void setRecTime(Integer recTime) {
+        RecTime = recTime;
+    }
+
+    public Integer getRecNum() {
+        return RecNum;
+    }
+
+    public void setRecNum(Integer recNum) {
+        RecNum = recNum;
+    }
+
+    public Integer getRecCal() {
+        return RecCal;
+    }
+
+    public void setRecCal(Integer recCal) {
+        RecCal = recCal;
+    }
+
+    public String getRecTag() {
+        return RecTag;
+    }
+
+    public void setRecTag(String recTag) {
+        RecTag = recTag;
+    }
+
+    public Integer getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(Integer userId) {
+        UserId = userId;
+    }
+
+    public Integer getRecLiked() {
+        return RecLiked;
+    }
+
+    public void setRecLiked(Integer recLiked) {
+        RecLiked = recLiked;
+    }
+
+    public String getRecCreated() {
+        return RecCreated;
+    }
+
+    public void setRecCreated(String recCreated) {
+        RecCreated = recCreated;
+    }
+
+    public Integer getRecViews() {
+        return RecViews;
+    }
+
+    public void setRecViews(Integer recViews) {
+        RecViews = recViews;
+    }
+
     //     配置 多對一 (Category表)
     @ManyToOne(cascade={CascadeType.MERGE})
     @JoinColumn(name = "CategoryId", referencedColumnName = "CategoryId",insertable = false, updatable = false)  // 外鍵關係設置
