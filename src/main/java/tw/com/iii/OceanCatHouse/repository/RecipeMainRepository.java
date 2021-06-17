e1250ededcb1cb5bae5185d9d0ea19ade3f0865e
Louis JJJ <louis82113@hotmail.com>
2021-06-17 13:33:24 +0800

    jian0617_1333

--- src/main/java/tw/com/iii/OceanCatHouse/repository/RecipeMainRepository.java
+++ src/main/java/tw/com/iii/OceanCatHouse/repository/RecipeMainRepository.java
@@ -5,8 +5,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import tw.com.iii.OceanCatHouse.model.RecipeMainBean;
 
+import java.util.List;
+
 public interface RecipeMainRepository extends JpaRepository<RecipeMainBean, Integer> {
 
     @Query("select count(m) from RecipeMainBean m where m.UserId = :userId")
     Integer recCount(@Param("userId") Integer userId);
+
+    @Query("from RecipeMainBean where UserId = :userId")
+    List<RecipeMainBean> findAllByUserid(@Param("userId") Integer userid);
 }
