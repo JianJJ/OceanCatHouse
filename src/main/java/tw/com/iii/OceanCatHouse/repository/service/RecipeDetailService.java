e97207be3d87b949671f8392eb28366fa036215d
Ceeport <ceeport.yu@gmail.com>
2021-06-20 20:09:42 +0800

    Yu:食譜組v2.99(Service微調)

--- src/main/java/tw/com/iii/OceanCatHouse/repository/service/RecipeDetailService.java
+++ src/main/java/tw/com/iii/OceanCatHouse/repository/service/RecipeDetailService.java
@@ -26,101 +26,97 @@ public class RecipeDetailService {
     private RecipeMaterialRepository recipeMatDao;
 
 
-   //1.訪問recipe_main，取得返回Bean
-    public RecipeMainBean getRecipeMainData(int id){
+    //1.訪問recipe_main，取得返回Bean
+    public RecipeMainBean getRecipeMainData(int id) {
         Optional<RecipeMainBean> ob = recipeMainDao.findById(id);
-        RecipeMainBean recipeData=ob.get();
+        RecipeMainBean recipeData = ob.get();
         return recipeData;
     }
 
     //2.訪問recipe_step，取得返回Bean(使用自定義查詢findByRecId)
-    public List<RecipeStepBean> getRecipeStepData(int recID){
+    public List<RecipeStepBean> getRecipeStepData(int recID) {
         List<RecipeStepBean> stepBean = recipeStepDao.findByRecId(recID);
         return stepBean;
     }
 
     //3.訪問recipe_material，取得返回Bean(使用自定義查詢findByRecId)
-    public List<RecipeMaterialBean> getRecipeMaterialData(int recID){
+    public List<RecipeMaterialBean> getRecipeMaterialData(int recID) {
         List<RecipeMaterialBean> matBean = recipeMatDao.findByRecId(recID);
         return matBean;
     }
 
     //4.推薦系統
-    public List<RecipeMainBean> getRecipeRecommend(int id,int recNum){
-        List<RecipeMainBean> recomBean = recipeMainDao.findByRecIdNot(id,recNum);
+    public List<RecipeMainBean> getRecipeRecommend(int id, int recNum) {
+        List<RecipeMainBean> recomBean = recipeMainDao.findByRecIdNot(id, recNum);
         return recomBean;
     }
 
     //5.搜尋系統01(以'食譜標題'搜尋)
-    public List<RecipeMainBean> getSearchResultMain(String searchString){
-        //關鍵字處理，拆分，合併:
-        String[] splitWord = searchString.split("");
-        String conbin = "";
-        if (!searchString.equals("")) {
-            for (int i = 0; i < splitWord.length; i++) {
-                conbin = conbin + splitWord[i] + "%";
-            }
-            conbin = conbin.substring(0, conbin.length() - 1);
-        }
+    public List<RecipeMainBean> getSearchResultMain(String searchString) {
+        //關鍵字處理，拆分，合併(關鍵字):
+        String conbinsearchKey = searchKeyWordProcess(searchString);
 
-        List<RecipeMainBean> searchBean = recipeMainDao.findByRecTitleContains(conbin);
-
-
-
-
+        List<RecipeMainBean> searchBean = recipeMainDao.findByRecTitleContains(conbinsearchKey);
         return searchBean;
     }
 
     //6.搜尋系統02(以'食材名稱'搜尋)
-    public List<RecipeMainBean> getSearchResultMat(String searchString){
-        //關鍵字處理，拆分，合併:
-        String[] splitWord = searchString.split(" ");
-        String conbin = "";
-        if (!searchString.equals("")) {
-            for (int i = 0; i < splitWord.length; i++) {
-                conbin = conbin + splitWord[i] + "%";
-            }
-            conbin = conbin.substring(0, conbin.length() - 1);
-        }
+    public List<RecipeMainBean> getSearchResultMat(String searchString) {
+        //關鍵字處理，拆分，合併(食材):
+        String conbinsearchMat = searchMatKeyWordProcess(searchString);
 
-
-        Set<Integer> beans = recipeMatDao.findByMaterialNameContains(conbin);
+        Set<Integer> beans = recipeMatDao.findByMaterialNameContains(conbinsearchMat);
         List<RecipeMainBean> recipeMainBeans = new ArrayList<>();
 
-        for(int recId : beans){
+        for (int recId : beans) {
             recipeMainBeans.add(getRecipeMainData(recId));
         }
-
         return recipeMainBeans;
     }
 
     //7.結合關鍵字與食材查詢
-    public List<RecipeMainBean> getSearchResultKeyAndMat(String searchKeyString,String searchMatString){
+    public List<RecipeMainBean> getSearchResultKeyAndMat(String searchKeyString, String searchMatString) {
         //關鍵字處理，拆分，合併(關鍵字):
-        String[] splitWord = searchKeyString.split("");
-        String conbinsearchKey = "";
-        if (!searchKeyString.equals("")) {
-            for (int i = 0; i < splitWord.length; i++) {
-                conbinsearchKey = conbinsearchKey + splitWord[i] + "%";
-            }
-            conbinsearchKey = conbinsearchKey.substring(0, conbinsearchKey.length() - 1);
-        }
+         String conbinsearchKey = searchKeyWordProcess(searchKeyString);
         //關鍵字處理，拆分，合併(食材):
-        String[] splitWord2 = searchMatString.split(" ");
-        String conbinsearchMat = "";
-        if (!searchMatString.equals("")) {
-            for (int i = 0; i < splitWord2.length; i++) {
-                conbinsearchMat = conbinsearchMat + splitWord2[i] + "%";
-            }
-            conbinsearchMat = conbinsearchMat.substring(0,conbinsearchMat.length()-1);
-        }
+         String conbinsearchMat = searchMatKeyWordProcess(searchMatString);
 
         List<RecipeMainBean> recipeMainBeans = new ArrayList<>();
         Set<Integer> recIdSet = recipeMainDao.findByRecTitleAndRecipeMaterialName(conbinsearchKey, conbinsearchMat);
-        for(int recId : recIdSet){
+        for (int recId : recIdSet) {
             recipeMainBeans.add(getRecipeMainData(recId));
         }
 
         return recipeMainBeans;
     }
+
+    //8.關鍵字處理工廠(關鍵字)
+    public String searchKeyWordProcess(String searchkeyWord) {
+        //關鍵字處理，拆分，合併(關鍵字):
+        String[] splitWord = searchkeyWord.split("");
+        String newKeyWord = "";
+        if (!searchkeyWord.equals("")) {
+            for (int i = 0; i < splitWord.length; i++) {
+                newKeyWord = newKeyWord + splitWord[i] + "%";
+            }
+            newKeyWord = newKeyWord.substring(0,newKeyWord.length() - 1);
+        }
+        return newKeyWord;
+    }
+
+    //9.關鍵字處理工廠(食材關鍵字)
+    public String searchMatKeyWordProcess(String searchkeyWord) {
+        //關鍵字處理，拆分，合併(食材名稱):
+        String[] splitWord = searchkeyWord.split(" ");
+        String newKeyWord = "";
+        if (!searchkeyWord.equals("")) {
+            for (int i = 0; i < splitWord.length; i++) {
+                newKeyWord = newKeyWord + splitWord[i] + "%";
+            }
+            newKeyWord = newKeyWord.substring(0,newKeyWord.length() - 1);
+        }
+        return newKeyWord;
+    }
+
+
 }
