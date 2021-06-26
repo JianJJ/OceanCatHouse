4b5c9f9d2b63fa4165035ab77d2e964152b1e7c1
zero71029 <wiz71029@gmail.com>
2021-06-26 11:23:27 +0800

    zero 0626 1100

--- src/main/java/tw/com/iii/OceanCatHouse/controller/ShopController.java
+++ src/main/java/tw/com/iii/OceanCatHouse/controller/ShopController.java
@@ -9,9 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import tw.com.iii.OceanCatHouse.Tool.ZeroTools;
 import tw.com.iii.OceanCatHouse.model.*;
@@ -199,5 +197,22 @@ public class ShopController {
         }
         return "redirect:/complete";
     }
+    // -------------Jian : 送出訂單, 新增訂單 ------------------------
+    @PostMapping("/insertOrder")
+    @ResponseBody
+    public String insertOrder(HttpSession session,
+                              @RequestBody List<ProductBean> productBeanList){
+        UserBean user = (UserBean) session.getAttribute("user");
+        Map<String, Integer> cat = (Map<String, Integer>) session.getAttribute("cat");
+        for (ProductBean apple : productBeanList){
+            System.out.println(apple);
+        }
+        OrdersBean ordersBean = ordersService.insertOrder(user, cat, productBeanList);
+        session.removeAttribute("cat");
+
+        return "新增成功";
+    }
 
 }
+
+
